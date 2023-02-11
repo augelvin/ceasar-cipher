83d79b66957a3025933bc29c91b4f5a725d995b6
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-02-11 15:01:53 -0800

    add text to number convertion

--- ceasar-cipher.rb
+++ ceasar-cipher.rb
@@ -1,3 +1,16 @@
-def ceasarCipher (text, key)
-
+def ceasar_cipher(text, key)
+    alphabet = [*('a'..'z')]
+    indices = []
+    text.downcase.chars.each do |character| 
+        if alphabet.include?(character)    
+            indices.push(alphabet.index(character))
+        else
+            indices.push(character)
+        end
+    end
+    indices
 end
+
+
+
+puts ceasar_cipher('What a string!', 2)
